504395b3aa83da215348adbbdb771472c96392b6
lee <china2011long@163.com>
2019-01-14 18:40:33 +0800

    添加命令支持

---
+++ module/cmd.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+function cmd_help() {
+    sed -n "s/^.*#HELP\\s//p;" < "$1" | sed "s/\\\\n/\n\t/g;s/$/\n/;s!CMD!${1/!/\\!}!g"
+    echo "显示帮助信息:"
+    echo "      $1 help"
+}
+
+cmdDict=()
+function init_cmdDict() {
+    if [ ${#cmdDict[*]} == 0 ]; then
+        #sed -n "s/^.*#HELP\\s//p;" < "$1" | awk '{print $2}'
+        cmdDict=`(sed -n "s/^.*#HELP\\s//p;" < "$1" | awk '{print $2}')`
+    fi
+}
+
+init_cmdDict $0
+
+# 调命令
+function call_cmd() {
+    [[ -z "${1-}" ]]
+    case $1 in
+        *) 
+    	for i in ${cmdDict[@]} 
+    	do
+    		if [ "$i" == "$1" ]; then
+                cmd_"$1" "${@:2}" 
+    			exit 0
+    		fi
+    	done
+        cmd_help "$0"
+        ;;
+    esac
+}
---
+++ test/cmd.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+. sloth-bootstrap.sh
+
+function cmd_test() { #HELP 重启服务器:\nCMD test <a1>
+    echo "lee"
+}
+
+function cmd_test1() { #HELP 重启服务器:\nCMD test1 <a1>
+    echo "lee1"
+}
+
+require "cmd"
+
+call_cmd "${@}"
+
