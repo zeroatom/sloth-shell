451834972ba4b6b1a2c801e487070c17389b4708
lee <china2011long@163.com>
2019-09-23 11:59:51 +0800

    change:颜色日志

--- module/log.sh
+++ module/log.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+COLOR_GREEN="\033[32m"
+COLOR_WARN="\033[31m\033[01m\033[05m"
+COLOR_TAIL="\033[0m"
+
 function log_info()
 {
 	local datetime=`date "+%Y-%m-%d %H:%M:%S"`
-	echo "[${datetime}] - $1"
+	echo -e "[${datetime}] - ${COLOR_GREEN}I${COLOR_TAIL} - $1"
+}
+
+function log_warn()
+{
+	local datetime=`date "+%Y-%m-%d %H:%M:%S"`
+	echo -e "[${datetime}] - ${COLOR_WARN}W${COLOR_TAIL} - $1"
 }
 
 cur_step=0
